5139602e3111811a69f7072cf1425ebc1173c68d
contreras-m <marcepazcc@gmail.com>
2025-09-03 14:28:13 +0200

    Add files via upload

---
+++ Lorenz_figure_script/run_lorenz.py
@@ -0,0 +1,90 @@
+import matplotlib
+import numpy as np
+import netCDF4
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import scipy.io as sio
+import xarray as xr
+import toolscroco as tlc
+import lorenz
+
+# Load Grid
+gname='/Users/contrema/Documents/PhD/Data/croco_grd.nc'
+
+
+ds = xr.open_dataset(gname)
+latC=ds['lat_rho'][:,:].data
+lonC=ds['lon_rho'][:,:].data
+pm=ds['pm'][:,:].data
+pn=ds['pn'][:,:].data
+maskC=ds['mask_rho'][:,:].data
+h=ds['h'][:,:].data
+ds.close()
+
+#Selection region depth than 200m
+posh=np.where(h<200)
+[M,L]=np.shape(maskC)
+posnan=np.where((maskC==0))
+masknan=np.ones((M,L))
+masknan[posnan]=np.nan
+masknan[posh]=np.nan
+
+
+
+#Variables Names
+k=0
+varname=["adk","adkB","bsk","sbk","adp","adpB","pkeK","pw","pwB","bsp","sbp","mpkeK"] #Name files
+varterm=["ADK","ADP","BSK","SBK","ADP","ADP","PKE","PW","PWB","BSP","SBP","MPKE"] #Name var (in the files)
+
+varnameF=["gB3","gS2","taubB","taubS","tauB","tauS","tauBA","tauSA","tauBG","tauSG"] #Name files
+vartermF=["gB3","gS2","taubB","taubS","tauB","tauS","tauBA","tauSA","tauBG","tauSG"]#Name var (in the files)
+
+
+mvar=np.zeros((len(varname)+len(varnameF)+8))
+    
+for it in range(len(varname)):
+
+    vart=varterm[it]
+    cgfile=("/Users/contrema/Documents/PhD/Data/3DAY/"+varname[it]+"_int.nc")
+    ds = xr.open_dataset(cgfile)
+    var=np.squeeze(ds[vart][:].data)*masknan
+    ds.close()
+    if (it==4) or (it==5) or (it==10) or (it==9):
+        mvar[it]=1e3*1025*np.nanmean(var[100:-100,100:-100])/1025
+    else:
+        mvar[it]=1e3*1025*np.nanmean(var[100:-100,100:-100])
+        
+kt=len(varname)
+for it in range(len(varnameF)):
+
+    vart=vartermF[it]
+    cgfile=("/Users/contrema/Documents/PhD/Data/3DAY/mean"+varnameF[it]+".nc")
+    if it==0 or it==1:
+        ds = xr.open_dataset(cgfile)
+        var1=np.squeeze(ds[(vart+"1")][:].data)*masknan
+        var2=np.squeeze(ds[(vart+"2")][:].data)*masknan
+        var=-var1+var2
+        ds.close()
+        mvar[kt]=(1025*1000)*np.nanmean(var[100:-100,100:-100])
+    else:
+        ds = xr.open_dataset(cgfile)
+        var=np.squeeze(ds[vart][:].data)*masknan
+        ds.close()
+        mvar[kt]=(1025*1000)*(np.nanmean(var[100:-100,100:-100])/1025)
+    kt=kt+1
+
+
+# Some variables have a negative sign (eg bottom drag) since in the estimation was not include.
+mvar[len(varname)+len(varnameF)]=((-mvar[7]+mvar[6])-mvar[2]-mvar[0]-mvar[6]); # FK_S+ DK_S
+mvar[len(varname)+len(varnameF)+1]=(-mvar[9]-mvar[4]+mvar[6]); # FP_S+ DP_S
+mvar[len(varname)+len(varnameF)+2]=(-(mvar[8]-mvar[11])+mvar[2]-mvar[1]-(mvar[3]+mvar[2])-mvar[11]); # FK_B+ DK_B
+mvar[len(varname)+len(varnameF)+3]=(-mvar[5]+mvar[11]+mvar[9]-(mvar[9]+mvar[10])); # FP_B+ DP_S
+##26
+mvar[len(varname)+len(varnameF)+4]=((-mvar[7]+mvar[6])-mvar[2]-mvar[0]-mvar[6])-mvar[17]+mvar[15]; #DK_S
+mvar[len(varname)+len(varnameF)+5]=(-mvar[9]-mvar[4]+mvar[6])-mvar[13]#DP_S
+mvar[len(varname)+len(varnameF)+6]=(-(mvar[8]-mvar[11])+mvar[2]-mvar[1]-(mvar[3]+mvar[2])-mvar[11])-mvar[16]+mvar[14]; #DK_B
+mvar[len(varname)+len(varnameF)+7]=(-mvar[5]+mvar[11]+mvar[9]-(mvar[9]+mvar[10]))-mvar[12] #DP_B
+
+lorenz.plot_lorenz_energy_cycle(mvar)
+
+
